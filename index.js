87033671e8f011b4135ba8ac28b8af71906f33ab
chesha1 <chesha1@163.com>
2025-01-18 10:23:54 +0800

    feat: 增加更多的 south-plus 域名

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         color-visited 对已访问过的链接染色
-// @version      1.4.7
+// @version      1.4.8
 // @description  把访问过的链接染色成灰色
 // @author       chesha1
 // @license      GPL-3.0-only
@@ -273,11 +273,13 @@
         },
         'south-plus': {
             pages: [
-                /https:\/\/www\.south-plus\.net\/thread\.php\?fid.*/,
-                /https:\/\/www\.south-plus\.net\/u\.php\?action-topic-uid-.*/,
+                /https:\/\/www\.(south|north|blue|snow)-plus\.net\/thread\.php\?fid.*/, // 板块首页
+                /https:\/\/bbs\.imoutolove\.me\/thread\.php\?fid.*/, // 板块首页
+                /https:\/\/www\.(south|north|blue)-plus\.net\/u\.php\?action-topic-uid-.*/, // 个人主页
             ],
             patterns: [
-                /south-plus\.net\/read\.php\?tid-.*/,
+                /(south|north|blue)-plus\.net\/read\.php\?tid-.*/, // 帖子
+                /bbs\.imoutolove\.me\/read\.php\?tid-.*/, // 帖子
 
             ]
         },
